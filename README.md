1baec797f09a72b65c4b32436338d9924120bd2a
rifkyfirdaus28 <rifkyfrds@student.telkomuniversity.ac.id>
2023-06-13 20:44:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# CI CD Pipeline using GIT Action
+
+This pipeline focuses on below jobs once code is pushed to GIT Main branch
+- Run unit test
+- Catch Bugs and Security Vulnerabilities in code Using Sonar (WIP)
+- Build Project
+- Deploy Project on AWS Elastic Bean Stack
+
---
+++ src/main/java/com/ebs/demo/ebsdemo/EBSDemoApplication.java
@@ -0,0 +1,23 @@
+package com.ebs.demo.ebsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EBSDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBSDemoApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<String> welcome(){
+		return new ResponseEntity<String>("Welcome to EBS Demo v1", HttpStatus.OK);
+	}
+
+}
